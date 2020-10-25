aacbd14f3a09616649a5f222be4dd73a02b0809e
Xzirez <futard@hotmail.no>
2020-10-25 20:58:19 +0100

    Some courses

---
+++ spring5-reactive-mongo-recipe-app-master/src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -0,0 +1,6 @@
+package guru.springframework.repositories.reactive;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository {
+}
